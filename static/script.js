2cf7e068aa033b12b24fcdc903e15538affce834
Luis Felipe <vilella.luisfelipe@gmail.com>
2022-12-07 23:40:10 -0300

    Add project template

---
+++ static/script.js
@@ -0,0 +1,33 @@
+new WOW().init();
+
+// import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+//
+// createApp({
+//     data() {
+//         return {
+//             message: 'Hello Vue!'
+//         }
+//     }
+// }).mount('#app')
+
+//
+// var app = new Vue({
+//     el: "#app",
+//     data: {
+//         message: "Hello from Vue!!",
+//     }
+// })
+// console.log('sup');
+
+// console.log('1')
+// const { createApp } = Vue
+// console.log('2')
+// console.log(createApp)
+//
+// createApp({
+//     data() {
+//         return {
+//             message: 'Hello Vue!'
+//         }
+//     }
+// }).mount('#app')
